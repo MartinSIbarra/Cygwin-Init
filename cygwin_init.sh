7d075e1b100ba1527c58910372408fab778ada66
MartinSIbarra <martinibarra@gmail.com>
2017-10-29 03:11:49 -0300

    Update cygwin_init.sh

--- cygwin_init.sh
+++ cygwin_init.sh
@@ -2,11 +2,11 @@
 clear
 echo "Obteniendo apt-cyg para instalar paquetes"
 cd ~
-lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg
 install apt-cyg /bin
 rm apt-cyg
 apt-cyg update
-apt-cyg install wget rsync git vim vim-common vim-doc vim-minimal vim-doc vim-lua
+apt-cyg install lynx rsync git vim vim-common vim-doc vim-minimal vim-doc vim-lua
 clear
 echo "Instalando terminal personalizada"
 curl -sSL https://raw.githubusercontent.com/MartinSIbarra/terminalrc/master/bin/shell_init_script.sh | bash
